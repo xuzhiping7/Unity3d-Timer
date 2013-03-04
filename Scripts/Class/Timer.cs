0d26f1eeaf0432b6d725009476fa7386e51c169b
xuzhiping <xuzhiping7@qq.com>
2013-03-04 20:32:14 +0800

    first update
    easy code and easy use !

---
+++ Scripts/Class/Timer.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+//Coded by ZhipingXu  xuzhiping7@qq.com 
+//Too simple, so I do not need to explain, just see the code. Help yourself.
+
+public class Timer{
+
+    //If the Timer is running 
+    private bool b_Tricking;
+
+    //Current time
+    private float f_CurTime;
+
+    //Time to reach
+    private float f_TriggerTime;
+
+    //Use delegate to hold the methods
+    public delegate void EventHandler();
+
+    //The trigger event list
+    public event EventHandler tick;
+
+    /// <summary>
+    /// Init
+    /// </summary>
+    /// <param name="second">Trigger Time</param>
+    public Timer(float second)
+    {
+        f_CurTime = 0.0f;
+        f_TriggerTime = second;
+    }
+    
+    /// <summary>
+    /// Start Timer
+    /// </summary>
+	public void Start()
+    {
+        b_Tricking = true;
+    }
+    
+    /// <summary>
+    /// Update Time
+    /// </summary>
+    public void Update()
+    {
+        if (b_Tricking)
+        {
+            f_CurTime += Time.deltaTime;
+
+            if (f_CurTime > f_TriggerTime)
+            {
+                tick();
+                b_Tricking = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Stop the Timer
+    /// </summary>
+    public void Stop()
+    {
+        b_Tricking = false;
+    }
+
+    /// <summary>
+    /// Continue the Timer
+    /// </summary>
+    public void Continue()
+    {
+        b_Tricking = true;
+    }
+
+    /// <summary>
+    /// Restart the this Timer
+    /// </summary>
+    public void Restart()
+    {
+        b_Tricking = true;
+        f_CurTime = 0.0f;
+    }
+
+    /// <summary>
+    /// Change the trigger time in runtime
+    /// </summary>
+    /// <param name="second">Trigger Time</param>
+    public void ResetTriggerTime(float second)
+    {
+        f_TriggerTime = second;
+    }
+}
---
+++ Scripts/Manager/TimerManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class TimerManager : MonoBehaviour
+{
+    Timer test;
+
+	// Use this for initialization
+	void Start () {
+        test = new Timer(3.0f);
+        test.tick += Test;
+        test.tick += Test2;
+        test.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        test.Update();
+	}
+
+    void Test()
+    {
+        Debug.Log("1");
+    }
+
+
+    void Test2()
+    {
+        Debug.Log("2");
+    }
+}
